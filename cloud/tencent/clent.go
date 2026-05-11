797c685a86cf0ede58e6bf7715d8900a8c5ee298
zzerd <zzerding@gmail.com>
2026-05-11 20:31:12 +0800

    refactor(tencent): remove global viper dependency from client

--- cloud/tencent/clent.go
+++ cloud/tencent/clent.go
@@ -3,57 +3,11 @@ package tencent
 import (
 	"fmt"
 	"sort"
-	"strings"
 
-	"github.com/spf13/viper"
 	cdn "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/cdn/v20180606"
 	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common"
 	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/profile"
-	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/regions"
 )
-
-func ReadConfig() (*Config, error) {
-	config := &Config{}
-	config.SecretID = viper.GetString("SECRET_ID")
-	config.SecretKey = viper.GetString("SECRET_KEY")
-	config.Region = viper.GetString("REGION")
-	if config.SecretID == "" || config.SecretKey == "" {
-		return nil, fmt.Errorf("SECRET_ID or SECRET_KEY is not set .you can set system env or use .env file")
-	}
-	if config.Region == "" {
-		config.Region = regions.Guangzhou
-	}
-	config.Name = "default"
-	return config, nil
-}
-
-func ReadConfigs() (map[string]Config, error) {
-	var sites map[string]Config
-	if err := viper.UnmarshalKey("sites", &sites); err == nil && len(sites) > 0 {
-		normalized := make(map[string]Config, len(sites))
-		for name, config := range sites {
-			siteName := strings.TrimSpace(name)
-			if siteName == "" {
-				return nil, fmt.Errorf("site name is empty")
-			}
-			if config.SecretID == "" || config.SecretKey == "" {
-				return nil, fmt.Errorf("site %q missing secret_id or secret_key", siteName)
-			}
-			if config.Region == "" {
-				config.Region = regions.Guangzhou
-			}
-			config.Name = siteName
-			normalized[siteName] = config
-		}
-		return normalized, nil
-	}
-
-	config, err := ReadConfig()
-	if err != nil {
-		return nil, err
-	}
-	return map[string]Config{config.Name: *config}, nil
-}
 
 func SortedSiteNames(configs map[string]Config) []string {
 	names := make([]string, 0, len(configs))
@@ -62,14 +16,6 @@ func SortedSiteNames(configs map[string]Config) []string {
 	}
 	sort.Strings(names)
 	return names
-}
-
-func CreateCDNClient() (*TencentCloudClient, error) {
-	config, err := ReadConfig()
-	if err != nil {
-		return nil, err
-	}
-	return CreateCDNClientForConfig(*config)
 }
 
 func CreateCDNClientForConfig(config Config) (*TencentCloudClient, error) {
